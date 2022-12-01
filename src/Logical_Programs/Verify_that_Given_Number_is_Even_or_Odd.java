02a8566e257a09de7d1d7128bd2948e9c31b8372
hd888 <hd888@Himanshu>
2022-12-01 12:29:47 +0530

    [HIMANSHU] : Verify Given Number is EVEN or ODD

---
+++ src/Logical_Programs/Verify_that_Given_Number_is_Even_or_Odd.java
@@ -0,0 +1,19 @@
+package Logical_Programs;
+
+public class Verify_that_Given_Number_is_Even_or_Odd 
+{
+	public static void main(String[]args)
+	{
+		int NUM = 19;
+		
+		if(NUM%2==0)
+		{
+			System.out.println(" Given Number "+NUM+" is EVEN");
+		}
+		else
+		{
+			System.out.println(" Given Number "+NUM+" is ODD");
+		}
+	}
+
+}
