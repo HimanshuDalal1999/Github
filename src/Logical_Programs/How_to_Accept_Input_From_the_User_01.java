1b14159536fcb5c80bbbe751a367daa73ac9aa23
hd888 <hd888@Himanshu>
2022-12-01 12:23:34 +0530

    HIMANSHU] Code for How to Accept the Input from the User

---
+++ src/Logical_Programs/How_to_Accept_Input_From_the_User_01.java
@@ -0,0 +1,26 @@
+package Logical_Programs;
+
+import java.util.Scanner;
+
+public class How_to_Accept_Input_From_the_User_01 
+{
+	public static void main(String[]args)
+	{
+//		System.out.println(" Enter Number : ");
+		Scanner S = new Scanner(System.in);
+//		int NUM = S.nextInt();
+//		System.out.println(NUM*NUM);
+		
+		System.out.println(" Enter Name : ");
+		String NAME = S.next();
+		System.out.println(NAME.length());
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/Sample/Demo.java
+++ src/Sample/Demo.java
@@ -1,5 +1,10 @@
 package Sample;
 
-public class Demo {
+public class Demo 
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" Himanshu Jarandhanji Dalal");
+	}
 
 }
